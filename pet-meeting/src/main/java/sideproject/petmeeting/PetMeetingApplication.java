9aa52395d2cb54db2df912d0f0177c43e81b62fd
BeomSeogKim <kbs4520@daum.net>
2022-11-15 15:17:14 +0900

    Initial Setting

---
+++ pet-meeting/src/main/java/sideproject/petmeeting/PetMeetingApplication.java
@@ -0,0 +1,13 @@
+package sideproject.petmeeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetMeetingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetMeetingApplication.class, args);
+    }
+
+}
