339dc2fa6d04f1c34d5d89abcffa45169f74ef0c
BeomSeogKim <kbs4520@daum.net>
2022-11-16 23:21:39 +0900

    회원가입 구현 #2

---
+++ pet-meeting/src/main/java/sideproject/petmeeting/member/dto/MemberDto.java
@@ -0,0 +1,19 @@
+package sideproject.petmeeting.member.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.validation.constraints.NotEmpty;
+
+@Builder
+@Getter
+public class MemberDto {
+    @NotEmpty
+    private String nickname;
+    @NotEmpty
+    private String password;
+    @NotEmpty
+    private String email;
+    @NotEmpty
+    private String image;
+}
