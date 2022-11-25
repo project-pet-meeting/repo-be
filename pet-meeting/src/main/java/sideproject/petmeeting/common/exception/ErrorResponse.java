5fc79e071b51c85d8c15bb80071acd2cbef91c85
kimskyyyy <hhhhh823@naver.com>
2022-11-26 01:13:33 +0900

    게시글 관련 예외 처리 추가 #3

--- pet-meeting/src/main/java/sideproject/petmeeting/common/exception/ErrorResponse.java
+++ pet-meeting/src/main/java/sideproject/petmeeting/common/exception/ErrorResponse.java
@@ -1,10 +1,7 @@
 package sideproject.petmeeting.common.exception;
 
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,48 +11,53 @@ import java.util.stream.Collectors;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ErrorResponse {
 
-    private int status;
+    private int statusCode;
     private String message;
-    private List<FieldError> errors;
     private String code;
+    private List<FieldError> errors;
+
 
 
     private ErrorResponse(final ErrorCode code, final List<FieldError> errors) {
+        this.statusCode = code.getStatusCode();
         this.message = code.getMessage();
-        this.status = code.getStatus();
-        this.errors = errors;
         this.code = code.getCode();
+        this.errors = errors;
+
     }
 
     private ErrorResponse(final ErrorCode code) {
+        this.statusCode = code.getStatusCode();
         this.message = code.getMessage();
-        this.status = code.getStatus();
         this.code = code.getCode();
         this.errors = new ArrayList<>();
+
     }
 
+    public ErrorResponse(final ErrorCode code, final String message) {
+        this.statusCode = code.getStatusCode();
+        this.message = message;
+        this.code = code.getCode();
+        this.errors = new ArrayList<>();
 
-//    public static ErrorResponse of(final ErrorCode code, final BindingResult bindingResult) {
-//        return new ErrorResponse(code, FieldError.of(bindingResult));
-//    }
+    }
+
+    public static ErrorResponse of(final ErrorCode code, final BindingResult bindingResult) {
+        return new ErrorResponse(code, FieldError.of(bindingResult));
+    }
 
     public static ErrorResponse of(final ErrorCode code) {
         return new ErrorResponse(code);
     }
 
-    public static ErrorResponse of(final ErrorCode code, final List<FieldError> errors) {
-        return new ErrorResponse(code, errors);
+    public static ErrorResponse of(ErrorCode code, String message) {
+        return new ErrorResponse(code, message);
+
     }
-
-//    public static ErrorResponse of(MethodArgumentTypeMismatchException e) {
-////        final String rejectValue = e.getValue() == null ? "" : e.getValue().toString();
-//        final List<ErrorResponse.FieldError> errors = ErrorResponse.FieldError.of(e.getName(), e.getErrorCode());
-//        return new ErrorResponse(ErrorCode.INVALID_TYPE_VALUE, errors);
-//    }
-
 
     @Getter
     @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @AllArgsConstructor
     public static class FieldError {
         private String field;
         private String objectName;
@@ -64,32 +66,17 @@ public class ErrorResponse {
         private String rejectValue;
 
 
-        private FieldError(final String field, final String objectName, final String code, final String defaultMessage, final String rejectValue) {
-            this.field = field;
-            this.objectName = objectName;
-            this.code = code;
-            this.defaultMessage = defaultMessage;
-            this.rejectValue = rejectValue;
+        private static List<FieldError> of(final BindingResult bindingResult) {
+            final List<org.springframework.validation.FieldError> fieldErrors = bindingResult.getFieldErrors();
+            return fieldErrors.stream()
+                    .map(error -> new FieldError(
+                            error.getField(),
+                            error.getObjectName(),
+                            error.getCode(),
+                            error.getDefaultMessage(),
+                            error.getRejectedValue() == null ? "" : error.getRejectedValue().toString()))
+                    .collect(Collectors.toList());
         }
-
-        public static List<FieldError> of(final String field, final String objectName, final String code, final String defaultMessage, final String rejectValue) {
-            List<FieldError> fieldErrors = new ArrayList<>();
-            fieldErrors.add(new FieldError(field, objectName, code, defaultMessage, rejectValue));
-            return fieldErrors;
-        }
-
-//        private static List<FieldError> of(final BindingResult bindingResult) {
-//            final List<org.springframework.validation.FieldError> fieldErrors = bindingResult.getFieldErrors();
-//            return fieldErrors.stream()
-//                    .map(error -> new FieldError(
-//                            error.getField(),
-//                            error.getObjectName(),
-//                            error.getCode(),
-//                            error.getDefaultMessage(),
-//                            error.getRejectedValue() == null ? "" : error.getRejectedValue().toString())
-//                    .collect(Collectors.toList());
-//        }
     }
-
 
 }
--- pet-meeting/src/main/java/sideproject/petmeeting/common/exception/GlobalExceptionHandler.java
+++ pet-meeting/src/main/java/sideproject/petmeeting/common/exception/GlobalExceptionHandler.java
@@ -3,13 +3,18 @@ package sideproject.petmeeting.common.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import org.springframework.web.multipart.support.MissingServletRequestPartException;
 
 import java.io.IOException;
 
-@RestControllerAdvice
+@RestControllerAdvice // Global Level using, 에러 전역 처리를 위한 어노테이션(Controller, Service 에서 throw 한 예외를 일괄 처리
 @Slf4j
 public class GlobalExceptionHandler {
 
@@ -24,25 +29,101 @@ public class GlobalExceptionHandler {
         final ErrorCode errorCode = e.getErrorCode();
         final ErrorResponse errorResponse = ErrorResponse.of(errorCode);
 
-        return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(errorCode.getStatus()));
+        return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(errorCode.getStatusCode()));
     }
 
     /**
-     * 시스템 에러
-     * javax.validation.Valid or @Validated 으로 binding error 발생시 발생한다.
-     * HttpMessageConverter 에서 등록한 HttpMessageConverter binding 못할경우 발생
-     * 주로 @RequestBody, @RequestPart 어노테이션에서 발생
+     * @Valid 유효성 체크에 통과하지 못하면 MethodArgumentNotValidException 발생
+     * @param e
+     * @return
      */
-//    @ExceptionHandler(MethodArgumentNotValidException.class)
-//    protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-//        log.error("handleMethodArgumentNotValidException", e);
-//        final ErrorResponse errorResponse = ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE, e.getBindingResult());
-//
-//        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
-//    }
-//
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.error("handleMethodArgumentNotValidException", e);
+        final ErrorResponse errorResponse = ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE, e.getBindingResult());
+        // Of 메서드:Null 값을 입력 받을 시 NullPointerException
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    /**
+     * Handler 에서 예외처리 되지 않은 Exception 처리
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<ErrorResponse> handleException(Exception e) {
+        log.error("handleEntityNotFoundException", e);
+        final ErrorResponse response = ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR);
+
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(IOException.class)
+    protected ResponseEntity<ErrorResponse> handleIOException(IOException e) {
+        log.error("handleIOException", e);
+        final ErrorResponse response = ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE);
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    /**
+     * RequestPart 데이터가 없을 시 발생하는 에러 예외 처리
+     */
+    @ExceptionHandler(MissingServletRequestPartException.class)
+    protected ResponseEntity<ErrorResponse> handleMissingServletRequestPartException(MissingServletRequestPartException e) {
+        log.error("handleMissingServletRequestPartException", e);
+        final ErrorResponse response = ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE, e.getMessage());
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    /**
+     * request parameter 가 없을 시 발생하는 에러 예외 처리
+     */
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    protected ResponseEntity<ErrorResponse> handleMissingServletRequestParameterException(MissingServletRequestParameterException e) {
+        log.error("handleMissingServletRequestParameterException", e);
+        final ErrorResponse response = ErrorResponse.of(ErrorCode.HANDLE_ACCESS_DENIED);
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    /**
+     * 이미지 파일 업로드 용량 초과 시 발생하는 에러 예외 처리
+     */
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    protected ResponseEntity<ErrorResponse> handleMaxUploadSizeExceededException(MaxUploadSizeExceededException e) {
+        log.error("handleMaxUploadSizeExceededException", e);
+        final ErrorResponse response = ErrorResponse.of(ErrorCode.FILE_SIZE_EXCEED);
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    /**
+     * 지원하지 않은 HTTP method 호출 시 발생하는 에러 예외 처리
+     */
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    protected ResponseEntity<ErrorResponse> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
+        log.error("handleHttpRequestMethodNotSupportedException", e);
+        final ErrorResponse response = ErrorResponse.of(ErrorCode.METHOD_NOT_ALLOWED);
+
+        return new ResponseEntity<>(response, HttpStatus.METHOD_NOT_ALLOWED);
+    }
+
+    /**
+     * 지원하지 않은 Content Type 으로 호출 시 발생하는 에러 예외 처리
+     */
+    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+    protected ResponseEntity<ErrorResponse> handleHttpMediaTypeNotSupportedException(HttpMediaTypeNotSupportedException e) {
+        log.error("handleHttpRequestMethodNotSupportedException", e);
+        final ErrorResponse response = ErrorResponse.of(ErrorCode.INVALID_TYPE_VALUE, e.getMessage());
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+
 //    /**
-//     * @ModelAttribut 으로 binding error 발생시 BindException 발생한다.
+//     * @ModelAttribute 으로 binding error 발생시 BindException 발생한다.
 //     * ref https://docs.spring.io/spring/docs/current/spring-framework-reference/web.html#mvc-ann-modelattrib-method-args
 //     */
 //    @ExceptionHandler(BindException.class)
@@ -55,7 +136,7 @@ public class GlobalExceptionHandler {
 //
 //    /**
 //     * enum type 일치하지 않아 binding 못할 경우 발생
-//     * 주로 @RequestParam enum으로 binding 못했을 경우 발생
+//     * 주로 @RequestParam enum 으로 binding 못했을 경우 발생
 //     */
 //    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
 //    protected ResponseEntity<ExceptionResponse> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e) {
@@ -65,20 +146,10 @@ public class GlobalExceptionHandler {
 //        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
 //    }
 //
-//    /**
-//     * 시스템 에러
-//     * 지원하지 않은 HTTP method 호출 할 경우 발생
-//     */
-//    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-//    protected ResponseEntity<ExceptionResponse> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
-//        log.error("handleHttpRequestMethodNotSupportedException", e);
-//        final ExceptionResponse response = ExceptionResponse.of(ErrorCode.METHOD_NOT_ALLOWED);
-//
-//        return new ResponseEntity<>(response, HttpStatus.METHOD_NOT_ALLOWED);
-//    }
+
 //
 //    /**
-//     * Authentication 객체가 필요한 권한을 보유하지 않은 경우 발생합
+//    * Authentication 객체가 필요한 권한을 보유하지 않은 경우 발생합
 //     */
 //    @ExceptionHandler(AccessDeniedException.class)
 //    protected ResponseEntity<ExceptionResponse> handleAccessDeniedException(AccessDeniedException e) {
@@ -88,23 +159,6 @@ public class GlobalExceptionHandler {
 //        return new ResponseEntity<>(response, HttpStatus.valueOf(ErrorCode.HANDLE_ACCESS_DENIED.getStatus()));
 //    }
 //
-//
-//
-//
-    @ExceptionHandler(Exception.class)
-    protected ResponseEntity<ErrorResponse> handleException(Exception e) {
-        log.error("handleEntityNotFoundException", e);
-        final ErrorResponse response = ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR);
 
-        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-
-    @ExceptionHandler(IOException.class)
-    protected ResponseEntity<ErrorResponse> handleIOException(Exception e) {
-        log.error("handleEntityNotFoundException", e);
-        final ErrorResponse response = ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR);
-
-        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
 
 }
