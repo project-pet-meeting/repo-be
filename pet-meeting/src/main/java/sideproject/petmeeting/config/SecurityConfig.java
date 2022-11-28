fd86f4cdbe117468cfe64b7a82e2c9140a7399de
BeomSeogKim <kbs4520@daum.net>
2022-11-28 16:20:08 +0900

    Email 인증 추가 #2

--- pet-meeting/src/main/java/sideproject/petmeeting/config/SecurityConfig.java
+++ pet-meeting/src/main/java/sideproject/petmeeting/config/SecurityConfig.java
@@ -67,7 +67,7 @@ public class SecurityConfig {
                 .sessionCreationPolicy(STATELESS)
                     .and()
                 .authorizeRequests()
-                .antMatchers("/api/member/signup", "/api/member/nickname", "/api/member/login").permitAll()
+                .antMatchers("/api/member/signup", "/api/member/nickname", "/api/member/login", "/api/member/emailConfirm").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
